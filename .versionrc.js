6ac763b45bbfd2d25b7a19ffed3bc44510bbafbf
ferhat elmas <elmas.ferhat@gmail.com>
2022-11-17 13:21:59 +0100

    chore: add ignored sections into changelog

--- .versionrc.js
+++ .versionrc.js
@@ -13,4 +13,14 @@ const versionFileUpdater = {
 
 module.exports = {
     bumpFiles: [{ filename: './lib/stream-chat/version.rb', updater: versionFileUpdater }],
+    types: [
+        {"type": "feat", "section": "Features"},
+        {"type": "fix", "section": "Bug Fixes"},
+        {"type": "chore", "section": "Other", "hidden": false},
+        {"type": "docs", "section": "Other", "hidden": false},
+        {"type": "style", "section": "Other", "hidden": false},
+        {"type": "refactor", "section": "Other", "hidden": false},
+        {"type": "perf", "section": "Other", "hidden": false},
+        {"type": "test", "section": "Other", "hidden": false}
+    ]
 }
