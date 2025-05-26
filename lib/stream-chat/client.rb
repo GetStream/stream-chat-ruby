a843cc007178a31c8b771f358d1bfdb68d96e6d2
Rafael Marinho <rafael.marinho@getstream.io>
2025-05-26 13:46:29 +0200

    add specs and fix location methods

--- lib/stream-chat/client.rb
+++ lib/stream-chat/client.rb
@@ -810,21 +810,22 @@ module StreamChat
     # Get active_live_locations for the current user
     #
     # @return [StreamChat::StreamResponse]
-    sig { returns(StreamChat::StreamResponse) }
-    def get_active_live_locations
-      get('users/locations')
+    sig { params(user_id: String).returns(StreamChat::StreamResponse) }
+    def get_active_live_locations(user_id)
+      get('users/locations', params: { user_id: user_id })
     end
 
     # Update live location
     #
+    # @param [String] user_id The ID of the user to update the location
     # @param [String] created_by_device_id The device ID that created the location
     # @param [String] message_id The message ID associated with the location
     # @param [Float] latitude Optional latitude coordinate
     # @param [Float] longitude Optional longitude coordinate
     # @param [String] end_at Optional end time for the location sharing
     # @return [StreamChat::StreamResponse]
-    sig { params(created_by_device_id: String, message_id: String, latitude: T.nilable(Float), longitude: T.nilable(Float), end_at: T.nilable(String)).returns(StreamChat::StreamResponse) }
-    def update_location(created_by_device_id:, message_id:, latitude: nil, longitude: nil, end_at: nil)
+    sig { params(user_id: String, created_by_device_id: String, message_id: String, latitude: T.nilable(Float), longitude: T.nilable(Float), end_at: T.nilable(String)).returns(StreamChat::StreamResponse) }
+    def update_location(user_id, created_by_device_id:, message_id:, latitude: nil, longitude: nil, end_at: nil)
       data = {
         created_by_device_id: created_by_device_id,
         message_id: message_id
@@ -833,7 +834,7 @@ module StreamChat
       data[:longitude] = longitude if longitude
       data[:end_at] = end_at if end_at
 
-      put('users/location', data: data)
+      put('users/location', data: data, params: { user_id: user_id })
     end
 
     # Gets a comamnd.
